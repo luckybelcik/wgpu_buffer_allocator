5228553a1b1168a2bcf14a6478af9efc1c0ab542
luckybelcik <luckybelcik@gmail.com>
2025-12-06 15:41:39 +0100

    Documentation changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # wgpu_buffer_allocator
+//!
+//! This crate provides a simple, robust memory allocator and abstraction layer
+//! for managing GPU buffers within wgpu applications.
+
 pub mod allocator;
 #[cfg(test)]
 pub mod util;
